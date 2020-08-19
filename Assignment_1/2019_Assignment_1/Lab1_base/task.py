48dfeec429536a7cc88181f35a47ff537fa5280f
Soumya Chatterjee <soumyac1999@gmail.com>
2020-08-19 13:24:46 +0530

    Added 2019 lab1

---
+++ Assignment_1/2019_Assignment_1/Lab1_base/task.py
@@ -0,0 +1,65 @@
+import numpy as np
+from utils import *
+
+def preprocess(X, Y):
+	''' TASK 0
+	X = input feature matrix [N X D] 
+	Y = output values [N X 1]
+	Convert data X, Y obtained from read_data() to a usable format by gradient descent function
+	Return the processed X, Y that can be directly passed to grad_descent function
+	NOTE: X has first column denote index of data point. Ignore that column 
+	and add constant 1 instead (for bias part of feature set)
+
+	Note : Remember to normalize input data before processing further
+	'''
+	pass
+
+def ordinary_least_squares(X, Y, lr=0.01):
+	''' TASK 2
+
+	X = input feature matrix [N X D]
+	Y = output values [N X 1]
+	Return the weight vector W, [D X 1] 
+	'''
+	pass
+
+def grad_ridge(W, X, Y, _lambda):
+	'''  TASK 3
+	W = weight vector [D X 1]
+	X = input feature matrix [N X D]
+	Y = output values [N X 1]
+	_lambda = scalar parameter lambda
+	Return the gradient of ridge objective function (||Y - X W||^2  + lambda*||w||^2 )
+	'''
+	pass
+
+def ridge_grad_descent(X, Y, _lambda, max_iter=30000, lr=0.00001, epsilon = 1e-4):
+	''' TASK 3 - PART A
+	X 			= input feature matrix [N X D]
+	Y 			= output values [N X 1]
+	_lambda 	= scalar parameter lambda
+	max_iter 	= maximum number of iterations of gradient descent to run in case of no convergence
+	lr 			= learning rate
+	epsilon 	= gradient norm below which we can say that the algorithm has converged 
+	Return the trained weight vector [D X 1] after performing gradient descent using Ridge Loss Function 
+	NOTE: You may precompure some values to make computation faster
+	'''
+	pass
+
+
+def coord_grad_descent(X, Y, _lambda, max_iter=1000):
+	''' TASK 4
+	X 			= input feature matrix [N X D]
+	Y 			= output values [N X 1]
+	_lambda 	= scalar parameter lambda
+	max_iter 	= maximum number of iterations of gradient descent to run in case of no convergence
+	Return the trained weight vector [D X 1] after performing gradient descent using Lasso Loss Function 
+	'''
+	pass
+
+
+if __name__ == "__main__":
+	X, Y = read_data("./dataset/train.csv")
+	X, Y = preprocess(X, Y)
+	trainX, trainY, testX, testY = separate_data(X, Y)
+	
