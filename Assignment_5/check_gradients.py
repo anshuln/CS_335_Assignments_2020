ad091e2c4c72e7460e25b205906a96b2082103f9
satvikmashkaria <satvikmashkaria@gmail.com>
2020-11-01 18:58:29 +0530

    Update check_gradients.py

--- Assignment_5/check_gradients.py
+++ Assignment_5/check_gradients.py
@@ -7,40 +7,79 @@ from layers import *
 
 np.random.seed(42)
 
-XTrain, YTrain, XVal, YVal, XTest, YTest = readCIFAR10()
-XTrain = XTrain[0:10, :, :, :]
-YTrain = YTrain[0:10, :]
+def check_fully_connected():
+	XTrain = np.random.randn(10, 100)
+	YTrain = np.random.randn(10, 10)
 
-nn1 = nn.NeuralNetwork(10, 1)
+	nn1 = nn.NeuralNetwork(10, 1)
+	nn1.addLayer(FullyConnectedLayer(100, 10, 'softmax'))
 
-nn1.addLayer(ConvolutionLayer([3, 32, 32], [3, 3], 4, 1, 'relu'))
-nn1.addLayer(AvgPoolingLayer([4, 30, 30], [2, 2], 2))
-nn1.addLayer(ConvolutionLayer([4, 15, 15], [4, 4], 4, 1, 'relu'))
-nn1.addLayer(MaxPoolingLayer([4, 12, 12], [2, 2], 2))
-nn1.addLayer(FlattenLayer())
-nn1.addLayer(FullyConnectedLayer(144, 10, 'softmax'))
+	delta = 1e-7
+	size = nn1.layers[0].weights.shape
+	num_grad = np.zeros(size)
 
-delta = 1e-7
-r = nn1.layers[0].weights.shape[2]
-c = nn1.layers[0].weights.shape[3]
-num_grad = np.zeros((r, c))
+	for i in range(size[0]):
+		for j in range(size[1]):
+			activations = nn1.feedforward(XTrain)
+			loss1 = nn1.computeLoss(YTrain, activations)
+			nn1.layers[0].weights[i, j] += delta
+			activations = nn1.feedforward(XTrain)
+			loss2 = nn1.computeLoss(YTrain, activations)
+			num_grad_ij = (loss2 - loss1) / delta
+			num_grad[i, j] = num_grad_ij
+			nn1.layers[0].weights[i, j] -= delta
 
-for i in range(r):
-	for j in range(c):
-		activations = nn1.feedforward(XTrain)
-		loss1 = nn1.computeLoss(YTrain, activations)
-		nn1.layers[0].weights[0, 0, i, j] += delta
-		activations = nn1.feedforward(XTrain)
-		loss2 = nn1.computeLoss(YTrain, activations)
-		num_grad_ij = (loss2 - loss1) / delta
-		num_grad[i, j] = num_grad_ij
-		nn1.layers[0].weights[0, 0, i, j] -= delta
+	saved = nn1.layers[0].weights[:, :].copy()
+	activations = nn1.feedforward(XTrain)
+	nn1.backpropagate(activations, YTrain)
+	new = nn1.layers[0].weights[:, :]
+	ana_grad = saved - new
 
-saved = nn1.layers[0].weights[0, 0, :, :].copy()
-activations = nn1.feedforward(XTrain)
-nn1.backpropagate(activations, YTrain)
-new = nn1.layers[0].weights[0, 0, :, :]
-ana_grad = saved - new
+	print(np.linalg.norm(num_grad - ana_grad))
+	assert np.linalg.norm(num_grad - ana_grad) < 1e-5
+	print("Gradient Test Passed for Fully Connected Layer!")
 
-assert np.linalg.norm(num_grad - ana_grad) < 1e-6
-print("Gradient Test Passed!")
+
+
+def check_all_layers():
+	XTrain = np.random.randn(10, 3, 32, 32)
+	YTrain = np.random.randn(10, 10)
+
+	nn1 = nn.NeuralNetwork(10, 1)
+
+	nn1.addLayer(ConvolutionLayer([3, 32, 32], [3, 3], 4, 1, 'relu'))
+	nn1.addLayer(AvgPoolingLayer([4, 30, 30], [2, 2], 2))
+	nn1.addLayer(ConvolutionLayer([4, 15, 15], [4, 4], 4, 1, 'relu'))
+	nn1.addLayer(MaxPoolingLayer([4, 12, 12], [2, 2], 2))
+	nn1.addLayer(FlattenLayer())
+	nn1.addLayer(FullyConnectedLayer(144, 10, 'softmax'))
+
+	delta = 1e-7
+	size = nn1.layers[0].weights.shape
+	num_grad = np.zeros(size)
+
+	for a in range(size[0]):
+		for b in range(size[1]):
+			for i in range(size[2]):
+				for j in range(size[3]):
+					activations = nn1.feedforward(XTrain)
+					loss1 = nn1.computeLoss(YTrain, activations)
+					nn1.layers[0].weights[a, b, i, j] += delta
+					activations = nn1.feedforward(XTrain)
+					loss2 = nn1.computeLoss(YTrain, activations)
+					num_grad_ij = (loss2 - loss1) / delta
+					num_grad[a, b, i, j] = num_grad_ij
+					nn1.layers[0].weights[a, b, i, j] -= delta
+
+	saved = nn1.layers[0].weights[:, :, :, :].copy()
+	activations = nn1.feedforward(XTrain)
+	nn1.backpropagate(activations, YTrain)
+	new = nn1.layers[0].weights[:, :, :, :]
+	ana_grad = saved - new
+
+	print(np.linalg.norm(num_grad - ana_grad))
+	assert np.linalg.norm(num_grad - ana_grad) < 1e-5
+	print("Gradient Test Passed for All layers!")
+
+check_fully_connected()
+check_all_layers()
